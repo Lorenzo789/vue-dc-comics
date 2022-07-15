e9926a4136b42e5c4e6dfca4467c7b557ac9a21a
Lorenzo789 <pallozzilorenzo@gmail.com>
2022-07-15 16:16:06 +0200

    dynamic header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,16 +4,11 @@
             <img src="../assets/img/dc-logo.png" alt="">
             <nav>
                 <ul>
-                    <li>Characters</li>
-                    <li>Comics</li>
-                    <li>Movies</li>
-                    <li>Tv</li>
-                    <li>Games</li>
-                    <li>Collectibles</li>
-                    <li>Videos</li>
-                    <li>Fans</li>
-                    <li>News</li>
-                    <li>Shop</li>
+                    <li v-for="(link, index) in menuHeaderLinks" :key="index">
+                        <a :href="link.url">
+                            {{ link.text }}
+                        </a>
+                    </li>
                 </ul>
             </nav>
         </div>
@@ -24,7 +19,53 @@
 <script>
 import HeaderContent from './HeaderContent.vue';
 export default {
-    components: { HeaderContent }
+    components: { HeaderContent },
+    data: function(){
+        return{
+            menuHeaderLinks: [
+                {
+                    text: 'Characters',
+                    url: '#',
+                },
+                {
+                    text: 'Comics',
+                    url: '#',
+                },
+                {
+                    text: 'Movies',
+                    url: '#',
+                },
+                {
+                    text: 'Tv',
+                    url: '#',
+                },
+                {
+                    text: 'Games',
+                    url: '#',
+                },
+                {
+                    text: 'Collectibles',
+                    url: '#',
+                },
+                {
+                    text: 'Videos',
+                    url: '#',
+                },
+                {
+                    text: 'Fans',
+                    url: '#',
+                },
+                {
+                    text: 'News',
+                    url: '#',
+                },
+                {
+                    text: 'Shop',
+                    url: '#',
+                },
+            ],
+        }
+    }
 }
 </script>
 
